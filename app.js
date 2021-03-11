a9d010c8d2b5b5a3e80b1ced2282ef2208a4bbf9
febanqueri <56729735+febanqueri@users.noreply.github.com>
2021-03-10 22:54:33 -0300

    Update - Noite

--- app.js
+++ app.js
@@ -110,7 +110,27 @@ function getTodos(){
     }else{
         todos = JSON.parse(localStorage.getItem("todo"));
     }
-    todos.forEach(function(todos)){
-        
-    }
+    todos.forEach(function(todo){
+        //Todo DIV
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        //Create LI
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todo;
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+
+        //CHECK MARK BUTTON
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+        //CHECK TRASH BUTTON
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+        //APPEND TO LIST
+        todoList.appendChild(todoDiv);
+    });
 }
